a986e398ba7780c4554ab6b9ee1f37a4a523eac0
paleblueyk <917885215@qq.com>
2022-04-16 01:01:51 +0800

    api

--- routers/v1/wx_router.go
+++ routers/v1/wx_router.go
@@ -1,10 +1,13 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/PaleBlueYk/randomSSQNumber/api"
+	"github.com/gin-gonic/gin"
+)
 
 func WxPusherRouter(router *gin.RouterGroup) {
 	r := router.Group("/wxpusher")
 	{
-		r.POST("/upload_info")
+		r.POST("/upload_info", api.UserUploadInfo)
 	}
 }
